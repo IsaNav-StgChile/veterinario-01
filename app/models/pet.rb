97a50215644bddf0a82ac3d04ab011ba542628ba
Isabel <isanpe@gmail.com>
2022-08-13 02:17:38 -0400

    Quinto commit agrega métodos al modelo pet y  agrega configuraciones a las  vistas

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,24 @@
 class Pet < ApplicationRecord
-    has_many :pet_histories    
+    has_many :pet_histories   
+    
+    def history_count
+        #TODO-implement
+      end
+    
+      def avg_weight
+        #TODO-implement
+      end
+    
+      def avg_height
+        #TODO-implement
+      end
+    
+      def max_weight
+        #TODO-implement
+      end
+    
+      def max_height
+        #TODO-implement
+      end    
+
 end
